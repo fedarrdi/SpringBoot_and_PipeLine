c77c4d653d0353417a718fc0c0428913171bbe65
Radoslav <fedarradi@gmail.com>
2026-06-26 11:51:14 +0300

    spring boot aplication fresh start

---
+++ healthapi/src/main/java/com/example/healthapi/HealthapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthapiApplication.class, args);
+	}
+
+}
